d3b1dc1853b0f2ca86d3bbcfc7e4e3696533fa95
Shelton Johnson <scj777@gmail.com>
2016-04-02 20:16:15 -0400

    update title

---
+++ index.php
@@ -0,0 +1,689 @@
+<!DOCTYPE html>
+
+<head>
+
+  <title>Shelton Johnson : Atlanta GA Front-End Web Developer</title>
+
+  <meta charset='utf-8'>
+
+  <meta name="description" content="Shelton Johnson, Atlanta Ga Front-End Web Developer For Hire">
+
+  <meta name="author" content="Shelton Johnson">
+
+  <meta content='width=device-width, initial-scale=1.0, user-scalable=yes' name='viewport'>
+
+  <meta name="google-site-verification" content="kZXai_hgE10gMIAK7bhfkF19euC5vlUClUI8DUgTz88" />
+
+  <link  rel="icon"  href="favicon.ico" />
+
+  <!-- PHP Includes  ############################################### -->
+
+  <?php include 'includes/functions.php';?>
+
+  <?php $xml = simplexml_load_file('projects_web.xml');?>
+
+  <!-- CSS ############################################### -->
+
+  <link href='css/style.css' media='all' rel='stylesheet' type='text/css'>
+
+  <link href='css/flexslider.css' media='all' rel='stylesheet' type='text/css'>
+
+  <link href='css/proxima.css' media='all' rel='stylesheet' type='text/css'>
+
+  <!-- FancyBox LIBs ############################################### -->
+
+  <!-- Add jQuery library -->
+
+  <script type="text/javascript" src="js/jquery/jquery-1.11.3.js"></script>
+
+  <!-- Add mousewheel plugin (this is optional) -->
+
+  <script type="text/javascript" src="/lib/fancybox/lib/jquery.mousewheel-3.0.6.pack.js"></script>
+
+  
+
+  <!-- Add fancyBox -->
+
+  <link rel="stylesheet" href="/lib/fancybox/source/jquery.fancybox.css?v=2.1.5" type="text/css" media="screen" />
+
+  <script type="text/javascript" src="/lib/fancybox/source/jquery.fancybox.pack.js?v=2.1.5"></script>
+
+
+
+
+
+  <!-- Optionally add helpers - button, thumbnail and/or media -->
+
+  <!-- <link rel="stylesheet" href="/lib/fancybox/source/helpers/jquery.fancybox-buttons.css?v=1.0.5" type="text/css" media="screen" />
+
+  // <script type="text/javascript" src="/lib/fancybox/source/helpers/jquery.fancybox-buttons.js?v=1.0.5"></script>
+
+  <script type="text/javascript" src="/lib/fancybox/source/helpers/jquery.fancybox-media.js?v=1.0.6"></script>
+
+  <link rel="stylesheet" href="/lib/fancybox/source/helpers/jquery.fancybox-thumbs.css?v=1.0.7" type="text/css" media="screen" />
+
+  <script type="text/javascript" src="/lib/fancybox/source/helpers/jquery.fancybox-thumbs.js?v=1.0.7"></script>
+
+ -->
+
+
+
+  <script type="text/javascript">
+
+  $(document).ready(function() {
+
+  $(".fancybox").fancybox();
+
+  });
+
+  </script>
+
+  <!-- End of LIBs ############################################### -->
+
+</head>
+
+<body>
+
+  <!-- Tracking Code ############################################### -->
+
+  <!-- Google Tag Manager -->
+
+  <noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-WKTLJT"
+
+  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+
+  <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+
+  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+
+  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+
+  '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+
+  })(window,document,'script','dataLayer','GTM-WKTLJT');</script>
+
+  <!-- End Google Tag Manager -->
+
+
+
+
+
+  <header class='navbar navbar-fixed-top'>
+
+    <div class='navbar-inner'>
+
+      <div class='container'>
+
+        <a class='sheltonj' href='index.php'><img src='images/sj-icon-small.png' alt="Shelton Johnson Logo"></a>
+
+        <?php include 'includes/menu.php';?>
+
+      </div>
+
+    </div>
+
+  </header>
+
+  
+
+  <div>
+
+    
+
+
+
+    <header id='slider'>
+
+      <div class="flexslider">
+
+        <ul class="slides">
+
+          <li>
+
+            <img src="img/slider-3c.jpg" alt="Web Developer for Hire">
+
+          </li>
+
+        </ul>
+
+      </div>
+
+    </header>
+
+    
+
+    
+
+    
+
+    <!-- BEGIN SECTION BIO  ############################################### -->
+
+    
+
+    <section class='short-wrap center' id='about'>
+
+      <div class='container'>
+
+        <header class='block-head'>
+
+          Web Design & Development
+
+          <p>Over 10 years experience in Development</p>
+
+        </header>
+
+        
+
+        
+
+        <div class='bio'>
+
+          <div class='text-block-about'>
+
+            <h1>About Shelton Johnson</h1>
+
+            <p>
+
+              Front End Web Developer with 12 years of experience working for a variety of retail companies like Meda Healthcare, iCRco, and LV.net. In my most recent position at Meda Healthcare, my main responsibilities included developing and maintaining the support of new and existing websites using HTML, CSS3,Javascript and JQuery. There, I also gained exposure working with web services to develop custom backend interface-to-monitor campaigns with PHP, MySql, and REST API. I have a strong business acumen, desire to grow in an enterprise environment and entrepreneurial mentality which will make me an asset on any team.
+
+            </p>
+
+          </div>
+
+          
+
+          
+
+          <div class='text-block'>
+
+            
+
+              <h2>Skills</h2>
+
+              <ul class="sidebar">
+
+
+
+            <li>HTML5, PHP, XML, XHTML, CSS3, XML, MySQL</li>
+
+            <li>Proficient in JavaScript and JQuery</li>
+
+            <li>Bootstrap, Less, Sass, Foundation ZURB, Git, API</li>
+
+            <li>Content Management Systems – Joomla (10 yrs), Wordpress (9 yrs)</li>
+
+            <li>Responsive Design, UX Design</li>
+
+            <li>eCommerce Solutions - Volusion and Magento Enterprise</li>
+
+            <li>Adobe Photoshop, Adobe Flash, Adobe Illustrator </li>
+
+            <li>Landing Page Design - Split Testing, Usability Testing</li>
+
+            <li>Search Engine Optimization (SEO) and Search Engine Marketing (SEM) </li>
+
+            <li>Google Adwords, Link Building, and Google A/B Testing </li>
+
+
+
+
+
+
+
+              </ul>
+
+              <br><br>
+
+           
+
+          </div>
+
+        </div>
+
+      </div>
+
+    </section>
+
+    <!-- AFTER BIO ############################################### -->
+
+    <section class='wrap bg2'>
+
+      <div class='container'>
+
+        <blockquote>
+
+          <p>Automation applied to an efficient operation will magnify the efficiency</p>
+
+          <cite>Bill Gates</cite>
+
+        </blockquote>
+
+      </div>
+
+    </section>
+
+    <!-- SECTION WORK ############################################### -->
+
+    <section class='wrap' id='work'>
+
+      <div class='container work'>
+
+        <header class='block-head'>
+
+          Selected Work
+
+          <p>Most recent work from <a href="#">the web</a></p>
+
+        </header>
+
+        <!--P[0] Vivarin ================================================= -->
+
+        <div class='project-grid'>
+
+          <div class='container'>
+
+            <div class='large columns'>
+
+              <div class='landscape'>
+
+                <a  class="fancybox" data-fancybox-group="portfolio" href='images/vi-full.jpg' >
+
+                  <img src='images/vi-slides.jpg' alt="Vivarin">
+
+                  <div>
+
+                    <div class='link-text hover'>
+
+                      <h2>Launch Website</h2>
+
+                    </div>
+
+                  </div>
+
+                </a>
+
+              </div>
+
+            </div>
+
+            <!--P[0] Vivarin ================================================= -->
+
+            
+
+            <div class='large columns'>
+
+              <div class='landscape'>
+
+                <a  class="fancybox" data-fancybox-group="portfolio" href='images/dr-slides.jpg' >
+
+                  <img src='images/dr-slides.jpg' alt="DrNatura">
+
+                  <div>
+
+                    <div class='link-text hover'>
+
+                      <h2>Launch Website</h2>
+
+                    </div>
+
+                  </div>
+
+                </a>
+
+              </div>
+
+            </div>
+
+            <!--P[0] Vmotion ================================================= -->
+
+            
+
+            <div class='large columns'>
+
+              <div class='landscape'>
+
+                <a  class="fancybox" data-fancybox-group="portfolio" href='images/portfolio/vMotion-Full.jpg' >
+
+                  <img src='images/vmotion-slides.jpg' alt="Vmotion">
+
+                  <div>
+
+                    <div class='link-text hover'>
+
+                      <h2><?php echo $xml->project[0]->name; ?></h2>
+
+                      
+
+                      
+
+                    </div>
+
+                  </div>
+
+                </a>
+
+              </div>
+
+            </div>
+
+            
+
+            
+
+            
+
+            <!--P[1] Clarity PACS ================================================= -->
+
+            <div class='large columns'>
+
+              <div class='landscape'>
+
+                <a class="fancybox" data-fancybox-group="portfolio" href='images/portfolio/Clarity-PACS-Final-Template.jpg'>
+
+                  <img   src='images/cp-slides.jpg' alt="">
+
+                  <div>
+
+                    <div class='link-text hover'>
+
+                      <h2><?php echo $xml->project[1]->name; ?></h2>
+
+                      
+
+                    </div>
+
+                  </div>
+
+                </a>
+
+              </div>
+
+            </div>
+
+            
+
+            
+
+            <!--P[2] iCRco.br  ===================================================  -->
+
+            <div class='large columns'>
+
+              <div class='landscape'>
+
+                <a class="fancybox" data-fancybox-group="portfolio" href='images/portfolio/icrcompany-com-br.jpg'>
+
+                  <img  src='images/icrco-br-slides.jpg' alt="">
+
+                  <div>
+
+                    <div class='link-text hover'>
+
+                      <h2><?php echo $xml->project[2]->name; ?></h2>
+
+                      <h3><?php echo $xml->project[2]->short_descr; ?></h3>
+
+                      <p><?php echo $xml->project[2]->descr; ?></p>
+
+                    </div>
+
+                  </div>
+
+                </a>
+
+              </div>
+
+            </div>
+
+
+
+            <!-- P[3] Cool-nSave ============================= -->
+
+<!--             <div class='large columns'>
+
+              <div class='landscape'>
+
+                <a class="fancybox" data-fancybox-group="portfolio"  href='images/portfolio/cns-slides.jpg'>
+
+                  <img src='images/cns-slides.jpg' alt="logo">
+
+                  <div>
+
+                    <div class='link-text hover'>
+
+                      <h2><?php echo $xml->project[3]->name; ?></h2>
+
+                      <h3><?php echo $xml->project[3]->short_descr; ?></h3>
+
+                      <p><?php echo $xml->project[3]->descr; ?></p>
+
+                    </div>
+
+                  </div>
+
+                </a>
+
+              </div>
+
+            </div> -->
+
+            <!--           <div class='small columns'>
+
+              <div class='portrait'>
+
+                <a class="fancybox" data-fancybox-group="portfolio"  href='images/295-cns.jpg'>
+
+                  <img src='images/295-cns.jpg' alt="logo">
+
+                  <div>
+
+                    <div class='link-text hover'>
+
+                      <h2><?php echo $xml->project[3]->name; ?></h2>
+
+                      <h3><?php echo $xml->project[3]->short_descr; ?></h3>
+
+                      <p><?php echo $xml->project[3]->descr; ?></p>
+
+                    </div>
+
+                  </div>
+
+                </a>
+
+              </div>
+
+            </div>
+
+            -->
+
+
+
+
+
+            <!-- P[4] Cert Fly  =============================
+
+            <div class='large columns'>
+
+              <div class='landscape'>
+
+                <a  class="fancybox" data-fancybox-group="portfolio"  href='images/cert-fly-slides.jpg'>
+
+                  <img src='images/cert-fly-slides.jpg' alt="Joomla">
+
+                  <div>
+
+                    <div class='link-text hover'>
+
+                      <h2><?php echo $xml->project[4]->name; ?></h2>
+
+                      <h3><?php echo $xml->project[4]->short_descr; ?></h3>
+
+                      <p><?php echo $xml->project[4]->descr; ?></p>
+
+                    </div>
+
+                  </div>
+
+                </a>
+
+              </div>
+
+            </div>
+
+            <div class='small columns'>
+
+              <div class='portrait'>
+
+                <a class="fancybox" data-fancybox-group="portfolio"  href='images/296-logo-fly.jpg'>
+
+                  <img src='images/296-logo-fly.jpg' alt="logo">
+
+                  <div>
+
+                    <div class='link-text hover'>
+
+                      <h2><?php echo $xml->project[4]->name; ?></h2>
+
+                      <h3><?php echo $xml->project[4]->short_descr; ?></h3>
+
+                      <p><?php echo $xml->project[4]->descr; ?></p>
+
+                    </div>
+
+                  </div>
+
+                </a>
+
+              </div>
+
+            </div>
+
+            -->
+
+          </div>
+
+        </div>
+
+      </div>
+
+    </section>
+
+    
+
+    <!-- AFTER WORK ############################################### -->
+
+    <!--       <section class='wrap bg2'>
+
+      <div class='container'>
+
+        <blockquote>
+
+          <p>“What we fear doing most is usually what we most need to do.”</p>
+
+          <cite> Timothy Ferriss, The 4-Hour Workweek</cite>
+
+        </blockquote>
+
+      </div>
+
+    </section> -->
+
+    <!-- SECTION CONTACT ############################################### -->
+
+    <section class='wrap' id='contact'>
+
+      <div class='container'>
+
+        <header class='block-head'>
+
+          Get in touch
+
+          <p></p>
+
+        </header>
+
+
+
+
+
+        <form id="contact-form" action="process.php" method="post">
+
+          <textarea id='message' maxlength='10000' name='message' placeholder='Tell us everything' required></textarea>
+
+          <div class='pencil-icon-o'></div>
+
+          <input class='text-input' id='senderEmail' maxlength='50' name='email' placeholder='E-mail' type='email' required>
+
+          <div class='email-icon'></div>
+
+          <input class='btn-input' id='sendMessage' name='sendMessage' type='submit' value='SUBMIT' >
+
+        </form>
+
+        <nav id="social">
+
+          <ul>
+
+            <li><a href="https://twitter.com/shelton_webdev" target="_blank" ><img src="images/icons/twitter.png" alt="Twitter"></a></li>
+
+            <li><a href="https://www.linkedin.com/in/sheltonj" target="_blank"><img src="images/icons/linked-in.png"  alt="Linkedin"></a></li>
+
+            <li><a href="skype:netdreamer09?call" target="_blank" ><img src="images/icons/skype.png" alt="Skype"></a></li>
+
+            <li><a href="https://github.com/sheltonjoomla" target="_blank" ><img src="images/icons/github.png" alt="GitHub"></a></li>
+
+          </ul>
+
+        </nav>
+
+
+
+        
+
+      </div>
+
+    </section>
+
+    
+
+    
+
+    
+
+    <section class='bye'>
+
+      <div class='container'>
+
+        <header class='block-head'>
+
+          <img src="img/sj-icon-small.png" alt="Logo Footer SheltonJ Forever "><p><?php auto_copyright('2010');   ?></p>
+
+        </header>
+
+      </div>
+
+    </section>
+
+  </div>
+
+  <!-- JAVASCRIPT ############################################### -->
+
+  <!-- Magnific Popup core JS file -->
+
+  <!-- <script src="lib/magnific-popup/jquery.magnific-popup.js"></script> -->
+
+
+
+  <!-- <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+
+  -->
+
+  <!-- <script src="js/jquery.flexslider.js"></script> -->
+
+  <!--  <script src="js/custom.js"></script> -->
+
+  
+
+</body>
+
+</html>
